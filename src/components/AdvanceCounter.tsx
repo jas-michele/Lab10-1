f0f2f8270bac2b99fa6ae173718e93132a554059
Jasmine <jasmineoliver158@gmail.com>
2026-04-30 16:12:57 -0400

    declared useState and useEffect

--- src/components/AdvanceCounter.tsx
+++ src/components/AdvanceCounter.tsx
@@ -5,4 +5,21 @@ export function AdvancedCounter() {
     const [count, setCount] = useState(0);
     const [step, setStep] = useState(1);
     const [history, setHistory] = useState<number[]>([]);
+
+    const add = () => {
+        setCount(prev => prev + step)
+    }
+
+    const minus = () => {
+        setCount(prev => prev - step )
+    }
+
+    const reset = () => {
+        setCount(0);
+        setHistory([]);
+    }
+
+    useEffect(() => {
+        setHistory(prev => [...prev, count])
+    }, [count]);
 }
